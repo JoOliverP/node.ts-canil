5d46469108d19c695f7d5de893064175022f6f76
JoOliverP <joypinheiro63@gmail.com>
2022-10-26 11:40:13 -0300

    base criada

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import doenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+import mainRoutes from './routes/index';
+
+doenv.config();
+
+const server = express();
+
+/* config template engine */
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+/*config pasta publica*/
+server.use(express.static(path.join(__dirname, '../public')))
+
+// ROTAS
+server.use(mainRoutes);
+
+server.use((req,res) => {
+   res.send('página não encontrada!')
+});
+
+
+
+
+server.listen(process.env.PORT);
